4254b4ad8627e126634a9e016175b43e6efb5f82
korsik993 <nick.korsik@yandex.ru@>
2023-06-17 17:51:17 +0300

    заполняет 3-хмерный массив уникальными значениями и выводит его

--- DZ/Program.cs
+++ DZ/Program.cs
@@ -9,68 +9,77 @@ namespace DZ
 {
     internal class Program
     {
-        static void rndArr(int[,] arr)
+        static void rndArr(int[,,] arr)
         {
             Random rnd = new Random();
+            bool exist;
+            int rndNew;
             for(int i = 0;  i < arr.GetLength(0); i++)
             {
                 for (int j = 0; j < arr.GetLength(1); j++)
                 {
-                    arr[i,j] = rnd.Next(1,5);
-                }
-            }
-        }
-
-        static void showArr(int[,] arr)
-        {
-            for (int i = 0; i < arr.GetLength(0); i++)
-            {
-                for (int j = 0; j < arr.GetLength(1); j++)
-                {
-                    Console.Write(arr[i,j] + "\t");
-                }
-                Console.WriteLine();
-            }
-        }
-
-        static int[,] multiArr(int[,] arr1, int[,] arr2, int side)
-        {
-            int[,] arrMult = new int[side, side];
-
-            for (int i = 0;i < side; i++)
-            {
-                for(int j = 0;j < side; j++)
-                {                                      
-                    for (int k = 0;k < side; k++)
+                    for (int k = 0; k < arr.GetLength(2);)
                     {
-                        arrMult[i, j] += arr1[i, k] * arr2[k, j];
+                        exist = false;
+                        rndNew = rnd.Next(10,100);
+
+                        for (int a = 0; a < arr.GetLength(0); a++)
+                        {
+                            for (int b = 0; b < arr.GetLength(1); b++)
+                            {
+                                for (int c = 0; c < arr.GetLength(2); c++)
+                                {
+                                    if (arr[a, b, c] == rndNew)
+                                    {
+                                        exist = true;
+                                        break;
+                                    }
+                                    if (exist == true)
+                                        break;
+                                }
+                                if (exist == true)
+                                    break;
+                            }
+                            if (exist == true)
+                                break;
+                        }
+
+                        if (!exist)
+                        {
+                            arr[i, j, k] = rndNew;
+                            k++;
+                        }
                     }
                 }
             }
-            return arrMult;
         }
 
+        static void showArr(int[,,] arr)
+        {
+            for(int i = 0;i < arr.GetLength(0); i++)
+            {
+                for(int j = 0; j < arr.GetLength(1); j++)
+                {
+                    for(int k = 0; k < arr.GetLength(2); k++)
+                    {
+                        Console.Write($"{arr[i,j,k]} ({i},{j},{k}) \t");
+                    }
+                    Console.WriteLine();
+                }
+                Console.WriteLine("-------------------------------------------");
+            }
+        }
+            
         static void Main(string[] args)
         {
-            Console.Write("Введите размер квадратной матрицы: ");
-            int side = int.Parse(Console.ReadLine());
+            Console.WriteLine("Размер трёхмерного массива будет [3,3,3] ");
+                      
 
-            int[,] arr1 = new int[side, side];
+            int[,,] arr = new int[3,3,3];
+            
+            rndArr(arr);
 
-            int[,] arr2 = new int[side, side];
-
-            rndArr(arr1);
-            rndArr(arr2);
-
-            Console.WriteLine("Матрица № 1");
-            showArr(arr1);
-            Console.WriteLine("Матрица № 2");
-            showArr(arr2);
-
-            int[,] multArr = multiArr(arr1 , arr2, side);
-
-            Console.WriteLine("Произведение 2-х квадратных матриц");
-            showArr(multArr);
+            showArr(arr);
 
             Console.ReadLine();
         }
